911ffb173725d888687f4152eafedee88bbb7e04
tinvukhac@gmail.com <tinvukhac@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-09-30 15:39:47 +0000

    Hoàn thành chức năng lập báo cáo "Xét nâng lương"

---
+++ trunk/SoureCode/QLCB_BK/QLCB_BK.BO/Report/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace QLCB_BK.BO.Report
+{
+    public class Report
+    {
+        public static DataTable GetSalaryIncrementList(int loaiHanNgach, int loaiBang, int thang, int nam)
+        {
+            return DAL.Report.Report.GetSalaryIncrementList(loaiHanNgach, loaiBang, thang, nam);
+        }
+    }
+}
---
+++ trunk/SoureCode/QLCB_BK/QLCB_BK.DAL/Report/Report.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using QLCB_BK.Common;
+
+namespace QLCB_BK.DAL.Report
+{
+    public class Report
+    {
+        private static string connectionString = @"Data Source=TINVUKHACPC\TINVUKHACSQL;Initial Catalog=hutstaff_9_8;Integrated Security=True";
+
+        // Method get salary increment list
+        public static DataTable GetSalaryIncrementList(int loaiHanNgach, int loaiBang, int thang, int nam)
+        {
+            string thoiGian;
+            if (thang < 10)
+            {
+                thoiGian = "0" + thang.ToString() + "/01/" + nam.ToString();
+            }
+            else
+            {
+                thoiGian = thang.ToString() + "/01/" + nam.ToString();
+            }
+            using (MainDB db = new MainDB(connectionString))
+            {
+                return db.Execute("[sp_report_get_salary_increment_list]",
+                    new string[] { "@loaiHanNgach", "@loaiBang", "@thoiGian" },
+                    new object[] { loaiHanNgach, loaiBang, thoiGian });
+            }
+        }
+    }
+}
