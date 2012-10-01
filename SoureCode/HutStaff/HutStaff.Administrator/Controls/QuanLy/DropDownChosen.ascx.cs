b3a00e0c72023c9c89691ae13c12751a3bfda322
nntoanbkit@gmail.com <nntoanbkit@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-01 14:25:23 +0000

    1. Sửa hutstaff_9_8.sql, thêm bản ghi rỗng ('') vào đầu 1 số bảng 2. Dựng form bổ sung cán bộ

---
+++ SoureCode/HutStaff/HutStaff.Administrator/Controls/QuanLy/DropDownChosen.ascx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HutStaff.Administrator.Controls.QuanLy
+{
+    public partial class DropDownChosen : System.Web.UI.UserControl
+    {
+        #region //--------- CLR Properties --------------//
+        public string DataSourceID
+        {
+            set
+            {
+                mainDropDownList.DataSourceID = value;
+            }
+        }
+
+        public string DataTextField
+        {
+            set { mainDropDownList.DataTextField = value; }
+        }
+
+        public string DataValueField
+        {
+            set { mainDropDownList.DataValueField = value; }
+        }
+
+        public string PlaceHolder { get; set; }
+
+        public Unit Width
+        {
+            get { return mainDropDownList.Width; }
+            set { mainDropDownList.Width = value; }
+        }
+
+        public Unit Height
+        {
+            get { return mainDropDownList.Height; }
+            set { mainDropDownList.Height = value; }
+        }
+
+        public string CssClass
+        {
+            get { return mainDropDownList.CssClass; }
+            set { mainDropDownList.CssClass = value; }
+        }
+        #endregion
+
+        #region //--------- Public Methods --------------//
+        public void AddItem(ListItem item)
+        {
+            mainDropDownList.Items.Add(item);
+        }
+
+        public void AddItems(ListItem[] items)
+        {
+            mainDropDownList.Items.AddRange(items);
+        }
+        #endregion
+
+        #region //--------- Event Handlers --------------//
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+    }
+}
