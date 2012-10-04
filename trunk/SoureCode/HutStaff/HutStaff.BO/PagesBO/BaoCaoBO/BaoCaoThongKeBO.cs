7b7fdf6cc9c05fa3246302303301317f1153c683
daubac403@gmail.com <daubac403@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-04 14:27:21 +0000

    update BO DAL

---
+++ trunk/SoureCode/HutStaff/HutStaff.BO/PagesBO/BaoCaoBO/BaoCaoThongKeBO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace HutStaff.BO.PagesBO.BaoCaoBO
+{
+    public class BaoCaoThongKeBO
+    {
+        public static DataTable ViewAlldm_dv()
+        {
+            return DAL.PagesDAL.BaoCaoDAL.BaoCaoThongKeDAL.ViewAlldm_dv();
+        }
+
+        public static DataTable ViewAlldm_dcb()
+        {
+            return DAL.PagesDAL.BaoCaoDAL.BaoCaoThongKeDAL.ViewAlldm_dcb();
+        }
+    }
+}
---
+++ trunk/SoureCode/HutStaff/HutStaff.DAL/PagesDAL/BaoCaoDAL/BaoCaoThongKeDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using HutStaff.Common;
+
+namespace HutStaff.DAL.PagesDAL.BaoCaoDAL
+{
+    public class BaoCaoThongKeDAL
+    {
+        public static DataTable ViewAlldm_dv()
+        {
+            using (MainDB db = new MainDB())
+            {
+                return db.Execute("sp_view_all_dm_dv", new string[] { }, new object[] { });
+            }
+        }
+
+        public static DataTable ViewAlldm_dcb()
+        {
+            using (MainDB db = new MainDB())
+            {
+                return db.Execute("sp_view_all_dm_dcb", new string[] { }, new object[] { });
+            }
+        }
+
+        
+    }
+}
