7f752a432637ed0246c5e79d255a343a20bf4db2
daubac403@gmail.com <daubac403@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-06 02:53:05 +0000

    update ALL

---
+++ SoureCode/HutStaff/HutStaff.BO/PagesBO/BaoCaoBO/KhenThuongBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace HutStaff.BO.PagesBO.BaoCaoBO
+{
+    public class KhenThuongBO
+    {
+        public static DataTable ViewAlldm_kt()
+        {
+            return DAL.PagesDAL.BaoCaoDAL.KhenThuongDAL.ViewAlldm_kt();
+        }
+    }
+}
