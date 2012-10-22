150820d3a504d0dd19ecd7f2cc492c82ecc10963
nghiabn1990 <nghiabn1990@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-22 17:57:34 +0000

    nghiapt

--- SoureCode/HutStaff/HutStaff.DAL/PagesDAL/QuanLy/BoSungCanBoDAL.cs
+++ SoureCode/HutStaff/HutStaff.DAL/PagesDAL/QuanLy/BoSungCanBoDAL.cs
@@ -10,7 +10,7 @@ using HutStaff.Common;
 
 namespace HutStaff.DAL.PagesDAL.QuanLy
 {
-    class BoSungCanBoDAL
+    public class BoSungCanBoDAL
     {
         public static DataTable getDienCanBo()
         {
@@ -28,5 +28,21 @@ namespace HutStaff.DAL.PagesDAL.QuanLy
             return dt;
         }
 
+
+        public static DataTable getDonVi()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MainDB db = new MainDB())
+                {
+                    dt = db.Execute("sp_view_all_dm_dv");
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            return dt;
+        }
     }
 }
