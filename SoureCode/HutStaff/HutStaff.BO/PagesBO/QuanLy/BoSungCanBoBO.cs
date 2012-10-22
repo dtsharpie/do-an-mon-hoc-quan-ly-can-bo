d48a5ac1341d1e406749b4da8e57012072d9be42
nghiabn1990 <nghiabn1990@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-22 16:37:24 +0000

    nghiapt

---
+++ SoureCode/HutStaff/HutStaff.BO/PagesBO/QuanLy/BoSungCanBoBO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using HutStaff.DAL.PagesDAL.QuanLy;
+
+namespace HutStaff.BO.PagesBO.QuanLy
+{
+    class BoSungCanBoBO
+    {
+        public static DataTable getDienCanBo()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = BoSungCanBoDAL.getDienCanBo();
+            }
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+            }
+            return dt;
+        }
+    }
+}
