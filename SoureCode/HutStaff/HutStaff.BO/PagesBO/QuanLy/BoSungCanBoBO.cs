c6f85159846142da7770eea8bc5b421c91930bda
nghiabn1990 <nghiabn1990@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-10-23 01:15:57 +0000

    nghiapt

--- SoureCode/HutStaff/HutStaff.BO/PagesBO/QuanLy/BoSungCanBoBO.cs
+++ SoureCode/HutStaff/HutStaff.BO/PagesBO/QuanLy/BoSungCanBoBO.cs
@@ -40,5 +40,56 @@ namespace HutStaff.BO.PagesBO.QuanLy
             }
             return dt;
         }
+
+        public static Dictionary<string, int> getQueHuong()
+        {
+            Dictionary<string, int> homeList = null;
+            Dictionary<string, int> province = null;
+            try
+            {
+                province = BoSungCanBoDAL.getTinh();
+            }
+            catch (Exception e)
+            {
+            }
+            if (province.Count > 0)
+            {
+                foreach(KeyValuePair<string, int> p in province){
+                    string provinceName = p.Key;    // ten Tinh
+                    int provinceID = p.Value;       // ID Tinh
+                    try
+                    {
+                        if (provinceID != 0)
+                        {
+                            DataTable dt = BoSungCanBoDAL.getHuyen(provinceID);
+                            foreach (DataRow r in dt.Rows)
+                            {
+                                string homeName;            // ten ket hop giua ten Tinh va Huyen, co dang: tenTinh - Ten Huyen
+                                int homeID;                 // = ma_huyen
+                                string districName = r["ten_huyen"].ToString();
+                                if ("".Equals(districName))
+                                {
+                                    homeName = provinceName;
+                                }
+                                else
+                                {
+                                    homeName = provinceName + " - " + r["ten_huyen"].ToString();
+                                }
+                                homeID = Int16.Parse(r["ma_huyen"].ToString());
+                                homeList.Add(homeName, homeID);
+                            }
+                        }
+                    }
+                    catch (Exception e) { }                    
+                }
+            }
+            return homeList;
+        }
+
+
+        public static Dictionary<string, int> getTinh()
+        {
+            return BoSungCanBoDAL.getTinh();
+        }
     }
 }
