6968c357d06b968d57dcb24a0d1856babc212174
tinvukhac@gmail.com <tinvukhac@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-11-17 11:22:00 +0000

    report

--- trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/Report.aspx.cs
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/Report.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using HutStaff.BO.PagesBO.BaoCaoBO;
+using HutStaff.BO.Report;
 using System.IO;
 
 namespace HutStaff.Administrator.Pages.BaoCao
@@ -14,6 +15,18 @@ namespace HutStaff.Administrator.Pages.BaoCao
     {
         private int typeOfPage = 0;
 
+        private IExportData _exporter;
+        private static string strHtmlContent = "";
+        private static bool isNotGottenHtmlContent = true;
+
+        public Report() : this(new ExportData()) { }
+
+        public Report(IExportData exporter)
+        {
+            _exporter = exporter;
+        }
+
+#region Set Up View
         protected void Page_Load(object sender, EventArgs e)
         {
             try
@@ -192,270 +205,27 @@ namespace HutStaff.Administrator.Pages.BaoCao
                 treeViewDonVi.Nodes.Add(node0);
             }
         }
-
+#endregion
         protected void btnXuat_Click(object sender, EventArgs e)
         {
-            BO.Report.Exporter.ExportToDoc(this.GetHtmlContent(), String.Format("b01_{0}.doc", DateTime.Now.ToString()));       
+            this.GetHtmlContent();
+            BO.Report.Exporter.ExportToDoc(strHtmlContent, String.Format("b01_{0}.doc", DateTime.Now.ToString()));       
         }
 
         protected void btnXem_Click(object sender, EventArgs e)
         {
-            lblResult.Text = Server.HtmlDecode(this.GetHtmlContent());
+            this.GetHtmlContent();
+            lblResult.Text = Server.HtmlDecode(strHtmlContent);
         }
 
-        private string GetHtmlContent()
+        private void GetHtmlContent()
         {
-            string strTempElement;
-            string elementPath = HttpContext.Current.Server.MapPath(@"Template\ReportElement_Type_1_1.xml");
-            string strElementHtmlContent = File.ReadAllText(elementPath);
-            string path = HttpContext.Current.Server.MapPath(@"Template\ReportHeader.xml");
-            string strHtmlContent = File.ReadAllText(path);
-            strHtmlContent = strHtmlContent.Replace("$TieuDe", "Bieu phan loai can bo cong chuc theo ngach, do tuoi va gioi tinh");
-            strHtmlContent = strHtmlContent.Replace("$TenDonVi", "Đại Học Bách Khoa Hà Nội");
-            strHtmlContent = strHtmlContent.Replace("$TenBaoCao", "BIỂU PHÂN LOẠI CÁN BỘ CÔNG CHỨC THEO NGẠCH, ĐỘ TUỔI VÀ GIỚI TÍNH");
-            strHtmlContent = strHtmlContent.Replace("$ThoiGianXet", DateTime.Now.ToString("dd/MM/yyyy"));
-
-            int stt = 0;
-            string maNgachHienTai = "";
-            string tenNgachHienTai = "";
-            DateTime dateTime;
-            int tuoi;
-            int nam30 = 0, nu30 = 0, nam31_40 = 0, nu31_40 = 0, nam41_45 = 0, nu41_45 = 0, nam46_50 = 0, nu46_50 = 0, nam51_55 = 0, nu51_55 = 0, nam56_60 = 0, nu56_60 = 0, nam60 = 0, nu60 = 0;
-
-            if (this.typeOfPage == 1)
+            if (isNotGottenHtmlContent)
             {
-                DataTable dataTable = BO.Report.Report.GetDataTableToReport_Type_1_1("0206", 1);
-
-                foreach (DataRow row in dataTable.Rows)
-                {
-
-                    if (stt == 0)
-                    {
-                        stt = 1;
-                        maNgachHienTai = row["ma_ngach"].ToString();
-                    }
-                    if (row["ma_ngach"].ToString() == maNgachHienTai)
-                    {
-                        tenNgachHienTai = row["ngach"].ToString();
-                        if (tenNgachHienTai.Length == 0)
-                        {
-                            tenNgachHienTai = "Không rõ";
-                        }
-                        dateTime = (DateTime)row["ntns"];
-                        tuoi = DateTime.Now.Year - dateTime.Year;
-                        if (tuoi <= 30)
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam30 += 1;
-                            }
-                            else
-                            {
-                                nu30 += 1;
-                            }
-                        }
-                        if ((tuoi > 30) && (tuoi < 41))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam31_40 += 1;
-                            }
-                            else
-                            {
-                                nu31_40 += 1;
-                            }
-                        }
-                        if ((tuoi > 40) && (tuoi < 46))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam41_45 += 1;
-                            }
-                            else
-                            {
-                                nu41_45 += 1;
-                            }
-                        }
-                        if ((tuoi > 45) && (tuoi < 51))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam46_50 += 1;
-                            }
-                            else
-                            {
-                                nu46_50 += 1;
-                            }
-                        }
-                        if ((tuoi > 50) && (tuoi < 56))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam51_55 += 1;
-                            }
-                            else
-                            {
-                                nu51_55 += 1;
-                            }
-                        }
-                        if ((tuoi > 55) && (tuoi < 61))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam56_60 += 1;
-                            }
-                            else
-                            {
-                                nu56_60 += 1;
-                            }
-                        }
-                        if (tuoi > 60)
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam60 += 1;
-                            }
-                            else
-                            {
-                                nu60 += 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        // In dong truoc
-                        strTempElement = strElementHtmlContent;
-                        strTempElement = strTempElement.Replace("$stt", stt.ToString());
-                        strTempElement = strTempElement.Replace("$ngach", tenNgachHienTai);
-                        strTempElement = strTempElement.Replace("$nam30", nam30.ToString());
-                        strTempElement = strTempElement.Replace("$nu30", nu30.ToString());
-                        strTempElement = strTempElement.Replace("$nam31_40", nam31_40.ToString());
-                        strTempElement = strTempElement.Replace("$nu31_40", nu31_40.ToString());
-                        strTempElement = strTempElement.Replace("$nam41_45", nam41_45.ToString());
-                        strTempElement = strTempElement.Replace("$nu41_45", nu41_45.ToString());
-                        strTempElement = strTempElement.Replace("$nam46_50", nam46_50.ToString());
-                        strTempElement = strTempElement.Replace("$nu46_50", nu46_50.ToString());
-                        strTempElement = strTempElement.Replace("$nam51_55", nam51_55.ToString());
-                        strTempElement = strTempElement.Replace("$nu51_55", nu51_55.ToString());
-                        strTempElement = strTempElement.Replace("$nam56_60", nam56_60.ToString());
-                        strTempElement = strTempElement.Replace("$nu56_60", nu56_60.ToString());
-                        strTempElement = strTempElement.Replace("$nam60", nam60.ToString());
-                        strTempElement = strTempElement.Replace("$nu60", nu60.ToString());
-                        strHtmlContent += strTempElement;
-
-                        // Tinh toan tiep
-                        stt++;
-                        maNgachHienTai = row["ma_ngach"].ToString();
-                        dateTime = (DateTime)row["ntns"];
-                        tuoi = DateTime.Now.Year - dateTime.Year;
-                        tenNgachHienTai = row["ngach"].ToString();
-                        nam30 = nu30 = nam31_40 = nu31_40 = nam41_45 = nu41_45 = nam46_50 = nu46_50 = nam51_55 = nu51_55 = nam56_60 = nu56_60 = nam60 = nu60 = 0;
-                        if (tuoi <= 30)
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam30 += 1;
-                            }
-                            else
-                            {
-                                nu30 += 1;
-                            }
-                        }
-                        if ((tuoi > 30) && (tuoi < 41))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam31_40 += 1;
-                            }
-                            else
-                            {
-                                nu31_40 += 1;
-                            }
-                        }
-                        if ((tuoi > 40) && (tuoi < 46))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam41_45 += 1;
-                            }
-                            else
-                            {
-                                nu41_45 += 1;
-                            }
-                        }
-                        if ((tuoi > 45) && (tuoi < 51))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam46_50 += 1;
-                            }
-                            else
-                            {
-                                nu46_50 += 1;
-                            }
-                        }
-                        if ((tuoi > 50) && (tuoi < 56))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam51_55 += 1;
-                            }
-                            else
-                            {
-                                nu51_55 += 1;
-                            }
-                        }
-                        if ((tuoi > 55) && (tuoi < 61))
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam56_60 += 1;
-                            }
-                            else
-                            {
-                                nu56_60 += 1;
-                            }
-                        }
-                        if (tuoi > 60)
-                        {
-                            if (row["gt"].ToString() == "1")
-                            {
-                                nam60 += 1;
-                            }
-                            else
-                            {
-                                nu60 += 1;
-                            }
-                        }
-                    }
-                }
-                strTempElement = strElementHtmlContent;
-                strTempElement = strTempElement.Replace("$stt", stt.ToString());
-                strTempElement = strTempElement.Replace("$ngach", tenNgachHienTai);
-                strTempElement = strTempElement.Replace("$nam30", nam30.ToString());
-                strTempElement = strTempElement.Replace("$nu30", nu30.ToString());
-                strTempElement = strTempElement.Replace("$nam31_40", nam31_40.ToString());
-                strTempElement = strTempElement.Replace("$nu31_40", nu31_40.ToString());
-                strTempElement = strTempElement.Replace("$nam41_45", nam41_45.ToString());
-                strTempElement = strTempElement.Replace("$nu41_45", nu41_45.ToString());
-                strTempElement = strTempElement.Replace("$nam46_50", nam46_50.ToString());
-                strTempElement = strTempElement.Replace("$nu46_50", nu46_50.ToString());
-                strTempElement = strTempElement.Replace("$nam51_55", nam51_55.ToString());
-                strTempElement = strTempElement.Replace("$nu51_55", nu51_55.ToString());
-                strTempElement = strTempElement.Replace("$nam56_60", nam56_60.ToString());
-                strTempElement = strTempElement.Replace("$nu56_60", nu56_60.ToString());
-                strTempElement = strTempElement.Replace("$nam60", nam60.ToString());
-                strTempElement = strTempElement.Replace("$nu60", nu60.ToString());
-                strHtmlContent += strTempElement;
-
-                string footerPath = HttpContext.Current.Server.MapPath(@"Template\ReportFooter.xml");
-                string footerHtmlContent = File.ReadAllText(footerPath);
-                footerHtmlContent = footerHtmlContent.Replace("$ThoiGianXet", DateTime.Now.ToString("dd/MM/yyyy"));
-                strHtmlContent += footerHtmlContent;
-
-                return strHtmlContent;
-            }
-            return "";
+                strHtmlContent = _exporter.GetHtmlContent_Report_Type_1_1("0206", "1", "1");
+                isNotGottenHtmlContent = false;
+            }          
         }
+
     }
 }
---
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.BO/Report/IExportData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HutStaff.BO.Report
+{
+    public interface IExportData
+    {
+        string GetHtmlContent_Report_Type_1_1(string madv, string dcb, string tt);
+    }
+}
