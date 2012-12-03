e32b71bcc0ea86e63ff6f8acc367f6d26600c82f
nghiabn1990 <nghiabn1990@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-12-03 01:32:36 +0000

    nghiapt

---
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.DAL/PagesDAL/TimKiem/SearchDetailDAL.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using HutStaff.Common;
+
+namespace HutStaff.DAL.PagesDAL.TimKiem
+{
+    public class SearchDetailDAL
+    {
+        public static DataTable ThongTinChiTiet(int iShcc)
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_find_by_id_soyeu",
+                    new string[] { "@shcc" },
+                    new object[]{iShcc});                
+            }
+            
+            return dt;
+        }
+
+        public static DataTable getAllTrinhDoHocVan()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tdhv",
+                    new string[] {},
+                    new object[] {});
+            }
+            return dt;
+        }
+
+        public static DataTable getAllTrinhDoTinHoc()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tdth",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllTrinhDoLiLuanChinhTri()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tdll",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllTrinhDoQuanLiNhaNuoc()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tdql",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllKhoiCanBo()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_kcb",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllDienCanBo()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_dcb",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllDonViQuanLi()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_dv",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllThuongBinh()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tb",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllGiaDinhChinhSach()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_gdcs",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllThanhPhanXuatThan()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tpxt",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllHuyen()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_dd",
+                    new string[] { },
+                    new object[] { });
+                foreach (DataRow r in dt.Rows)
+                {
+                    int ma_ttp = int.Parse(r["ma_tinh"].ToString());
+                    string ten_ttp = db.Execute("sp_find_by_id_dm_ttp",
+                                                new string[]{"@ma_ttp"},
+                                                new object[]{ma_ttp}).Rows[0]["ttp"].ToString();
+                    if ("".Equals(r["ten_huyen"]))
+                    {
+                        r["ten_huyen"] = ten_ttp;
+                    }
+                    else
+                    {
+                        r["ten_huyen"] = r["ten_huyen"] + " - " + ten_ttp;
+                    }
+                }
+            }
+            return dt;
+        }
+
+        public static DataTable getAllTinhTrangHonNhan()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tthn",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllDanToc()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_dt",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+
+        public static DataTable getAllTonGiao()
+        {
+            DataTable dt = new DataTable();
+            using (MainDB db = new MainDB())
+            {
+                dt = db.Execute("sp_view_all_dm_tg",
+                    new string[] { },
+                    new object[] { });
+            }
+            return dt;
+        }
+    }
+}
