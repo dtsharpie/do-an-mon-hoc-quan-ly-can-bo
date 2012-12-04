31ba01fbe304ed0e239f2a20aee91dbc18035290
tinvukhac@gmail.com <tinvukhac@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-12-04 03:21:51 +0000

    Danh sach nang luong

---
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/DanhSachNangLuong.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace HutStaff.Administrator.Pages.BaoCao
+{
+    public partial class DanhSachNangLuong : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = HttpContext.Current.Server.MapPath(@"Template\DanhSachNangLuongHeader.xml");
+            string strHtmlContent = File.ReadAllText(path);
+            lblHtmlContent.Text = strHtmlContent;
+        }
+    }
+}
