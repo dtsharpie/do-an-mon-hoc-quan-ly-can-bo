8bd112438812c9e44f95efef693ecaeb0cf8ddf9
tinvukhac@gmail.com <tinvukhac@gmail.com@d8cc9390-56f8-a242-15a3-192b1157e5eb>
2012-12-07 16:28:34 +0000

    In quyết định đề nghị nâng lương.

--- trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/DanhSachNangLuong.aspx.cs
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/DanhSachNangLuong.aspx.cs
@@ -5,14 +5,63 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using HutStaff.BO.Report;
+using System.Data;
 
 namespace HutStaff.Administrator.Pages.BaoCao
 {
     public partial class DanhSachNangLuong : System.Web.UI.Page
     {
+        private DataTable dt1;
+        private DataTable dt2;
+        private DataTable dt3;
+
+        private static string donvi = "";
+        private static string dcb = "";
+        private static string tt = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            donvi = Request.QueryString["donvi"];
+            dcb = Request.QueryString["dcb"];
+            tt = Request.QueryString["tt"];
+            dt1 = BO.Report.Report.GetDataTableToReport_Type_3_1(donvi, dcb, tt);
+            dt2 = BO.Report.Report.GetDataTableToReport_Type_3_2(donvi, dcb, tt);
+            dt3 = BO.Report.Report.GetDataTableToReport_Type_3_3(donvi, dcb, tt);
+
+            DataColumn orderColumn = dt1.Columns.Add("STT");
+            orderColumn.SetOrdinal(0);
+            int order = 0;
+            foreach (DataRow row in dt1.Rows)
+            {
+                order++;
+                row["STT"] = order.ToString();
+            }
+
+            DataColumn orderColumn2 = dt2.Columns.Add("STT");
+            orderColumn2.SetOrdinal(0);
+            int order2 = order;
+            foreach (DataRow row in dt2.Rows)
+            {
+                order2++;
+                row["STT"] = order2.ToString();
+            }
+
+            DataColumn orderColumn3 = dt3.Columns.Add("STT");
+            orderColumn3.SetOrdinal(0);
+            int order3 = order2;
+            foreach (DataRow row in dt3.Rows)
+            {
+                order3++;
+                row["STT"] = order3.ToString();
+            }
+
+            Repeater1.DataSource = dt1;
+            Repeater1.DataBind();
+            Repeater2.DataSource = dt2;
+            Repeater2.DataBind();
+            Repeater3.DataSource = dt3;
+            Repeater3.DataBind();
         }
     }
 }
--- trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/Report2.aspx.cs
+++ trunk/SoureCode/Version2/HutStaff/HutStaff.Administrator/Pages/BaoCao/Report2.aspx.cs
@@ -11,6 +11,10 @@ namespace HutStaff.Administrator.Pages.BaoCao
 {
     public partial class Report2 : System.Web.UI.Page
     {
+        private static string donvi = "";
+        private static string dcb = "";
+        private static string tt = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
@@ -38,7 +42,31 @@ namespace HutStaff.Administrator.Pages.BaoCao
 
         protected void btnBuildList_Click(object sender, EventArgs e)
         {
-            Response.Redirect("./DanhSachNangLuong.aspx");
+            donvi = ddlDonViLapBaoCao.SelectedItem.Value;
+
+            dcb = "";
+            foreach (ListItem li in chkboxDienCanBo.Items)
+            {
+                if (li.Selected == true)
+                {
+                    dcb += li.Value + ",";
+                }
+            }
+            if (dcb.Length > 0)
+                dcb = dcb.Substring(0, dcb.Length - 1);
+
+            tt = "";
+            foreach (ListItem li in chkboxTinhTrangHienTai.Items)
+            {
+                if (li.Selected == true)
+                {
+                    tt += li.Value + ",";
+                }
+            }
+            if (tt.Length > 0)
+                tt = tt.Substring(0, tt.Length - 1);
+
+            Response.Redirect(String.Format("./DanhSachNangLuong.aspx?donvi={0}&dcb={1}&tt={2}", donvi, dcb, tt));
         }
     }
 }
